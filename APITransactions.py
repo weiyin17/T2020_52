8647e64bc9dd59751500b92d5ef47e3846e832d3
ZiQiang <1802203@sit.singaporetech.edu.sg>
2020-01-07 16:41:56 +0800

    Edit to calculate transport expenses of users
    Send marketing messages to user based on transport expenses from past 2 years

--- APITransactions.py
+++ APITransactions.py
@@ -2,7 +2,7 @@ import requests
 import json
 import matplotlib.pyplot as plt
 
-url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/transactions/10?from=01-01-2018&to=12-31-2018"
+url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/transactions/94?from=01-01-2018&to=01-30-2020"
 
 payload = {}
 headers = {
@@ -17,7 +17,14 @@ json_data = json.loads(response.content)
 credamt = 0
 debitamt = 0
 tagamt = []
-transtag = ['TRANSFER', 'ONLINE', 'F&B', 'ATM', 'LEISURE', 'TRANSPORT'], tagamt
+transtag = ['TRANSFER', 'ONLINE', 'F&B', 'ATM', 'LEISURE', 'TRANSPORT']
+# transferamt = 0
+# onlineamt = 0
+# fandbamt = 0
+# atmamt = 0
+# leisureamt = 0
+transportamt = 0
+
 
 
 for i in range(len(json_data)):
@@ -34,24 +41,25 @@ for i in range(len(json_data)):
     cattag = json_data[i]['tag']
     # df = pd.read_json(json_data)
     # print(df)
+    # Recommend sending marketing messages for transport based on transport expenses
+    for r in range(len(transtag)):
+        if cattag == "TRANSPORT":
+            transportamt = transportamt + round(float(amt), 2)
 
-    # for r in range(len(transtag)):
-    #     if cattag == transtag[r]:
-    #         transtag[r].append(amt)
 
-print(transtag)
+print('Transport: $' + '%.2f' % transportamt)
 labels = ['Credit', 'Debit']
 amt = [credamt, debitamt]
-print('Credit Amount: ' + '%.2f' % credamt)
-print('Debit Amount: ' + '%.2f' % debitamt)
+print('Credit Amount: $' + '%.2f' % credamt)
+print('Debit Amount: $' + '%.2f' % debitamt)
 savings = credamt - debitamt
-print('Savings accrued: ' + '%.2f' % savings)
+print('Savings accrued: $' + '%.2f' % savings)
 plt.pie(amt, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 plt.legend(labels,
           title="Credit / Debit",
           loc="best")
-plt.title('2018 Transaction Summary')
+plt.title('2018 - 2020 Transaction Summary')
 plt.axis('equal')
 plt.show()
 
